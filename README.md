465bc78293d90e91333c529a7b99eec3bf9f4433
Le Xuan Long <longle@beeketing.net>
2021-07-07 23:14:27 +0700

    add terraform gcp example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # terraform_iac
 terraform infra as code labs
+
+# terraform commands
+
+### terraform init
+
+### terraform plan
+
+### terrafom apply
+
+### terraform destroy
---
+++ terraform_gcp/gcp_postgresql.tf
@@ -0,0 +1,21 @@
+provider "google" {
+ credentials = file("../credentials/techmaster-04d5d76222fb.json")
+ project     = "techmaster-305714"
+ region      = "us-west1"
+}
+
+resource "google_sql_database_instance" "pg-01" {
+  name             = "pg-01"
+  database_version = "POSTGRES_11"
+  region           = "us-west1"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+# resource "google_sql_user" "users" {
+#   name     = "app-01"
+#   instance = google_sql_database_instance.pg-01.name
+#   password = "change-me-now"
+# }
