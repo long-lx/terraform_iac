eec8813f84d22f20559253c38c56c45f16cb8984
Le Xuan Long <longle@beeketing.net>
2021-07-13 22:04:34 +0700

    update

--- terraform_aws/aws_ec2.tf
+++ terraform_aws/aws_ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
 	ami = "ami-06fb5332e8e3e577a"
-	instance_type = "t2.micro"
+	instance_type = "t2.large"
 
 	tags = {
 		Name = "web-0"
--- terraform_modules/testing/main.tf
+++ terraform_modules/testing/main.tf
@@ -10,3 +10,11 @@ module "web" {
   ec2_ami             = "ami-06fb5332e8e3e577a"
   ec2_instance_type   = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-s3-state-techmaster-2021-devops-long"
+    key    = "testing/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
