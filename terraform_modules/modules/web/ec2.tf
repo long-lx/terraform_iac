ee7ea0dcca2e90d41cbbbd5f9a763b24d7968cbf
Le Xuan Long <longle@beeketing.net>
2021-07-18 12:08:41 +0700

    update lab ansible

--- terraform_modules/modules/web/ec2.tf
+++ terraform_modules/modules/web/ec2.tf
@@ -35,6 +35,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks      = ["14.232.243.111/32"]
   }
 
+  ingress {
+    description      = "HTTP from specify IPs"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["14.232.243.111/32"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
